290f719d4c32c87d2914f862f5da21baf5c8244b
Sergey Archipov <sergey.archipov.jp@gmail.com>
2017-03-07 13:04:00 +0200

    Basic functionality interface

---
+++ IBasicFunctionality.java
@@ -0,0 +1,15 @@
+
+public interface IBasicFunctionality {
+
+    public double add();
+
+    public double subtract();
+
+    public double multiply();
+
+    public double divide();
+
+    public double power();
+
+
+}
